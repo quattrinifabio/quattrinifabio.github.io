4b36927e8606ff299cee6b939c94f44db51e7a05
Fabio Quattrini <38918787+quattrinifabio@users.noreply.github.com>
2025-05-27 15:45:36 +0200

    image viewer commented out

--- src/components/portfolio-entry.tsx
+++ src/components/portfolio-entry.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
+// import { useState } from "react";
 import { ArrowUpRight } from "lucide-react";
 import { Portfolio } from "@/data/portfolio";
 // import { ImageViewer } from "./image-viewer";
